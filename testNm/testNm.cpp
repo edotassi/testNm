19ed46e24fc32ae8d5b08d2aeb0ee338527936c7
Edoardo <linuxtassi@gmail.com>
2013-01-11 19:18:22 +0100

    first commit

---
+++ testNm/testNm.cpp
@@ -0,0 +1,74 @@
+// testNm.cpp : definisce il punto di ingresso dell'applicazione console.
+//
+
+#include "stdafx.h"
+#include "windows.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "objbase.h"
+#include "ntddndis.h"
+#include "NMApi.h"
+
+
+void __stdcall 
+	MyFrameIndication(HANDLE hCapEng, ULONG ulAdaptIdx, PVOID pContext, HANDLE hRawFrame)
+{
+	HANDLE capFile = (HANDLE)pContext;
+	NmAddFrame(capFile, hRawFrame);
+}
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	ULONG ret;
+	ULONG adapterIndex = 0;
+
+	if(2 == argc)
+		adapterIndex = _wtol(argv[1]);
+
+	// Open a capture file for saving frames.
+	HANDLE myCapFile;
+	ULONG CapSize;
+	ret = NmCreateCaptureFile(L"20sec.cap", 20000000, NmCaptureFileWrapAround, &myCapFile, &CapSize);
+	if(ret != ERROR_SUCCESS)
+	{
+		wprintf(L"Error opening capture file, 0x%X\n", ret);
+		return ret;
+	}
+
+	// Open the capture engine.
+	HANDLE myCaptureEngine;
+	ret = NmOpenCaptureEngine(&myCaptureEngine);
+	if(ret != ERROR_SUCCESS)
+	{
+		wprintf(L"Error opening capture engine, 0x%X\n", ret);
+		NmCloseHandle(myCapFile);
+		return ret;
+	}
+
+	//Configure the adapter.
+	ret = NmConfigAdapter(myCaptureEngine, adapterIndex, MyFrameIndication, myCapFile);
+	if(ret != ERROR_SUCCESS)
+	{
+		wprintf(L"Error configuration adapter, 0x%X\n", ret);
+		NmCloseHandle(myCaptureEngine);
+		NmCloseHandle(myCapFile);
+		return ret;
+	}
+
+	//Start capturing frames.
+	wprintf(L"Capturing for 20 seconds\n");
+	NmStartCapture(myCaptureEngine, adapterIndex, NmLocalOnly);
+
+	Sleep(20000);
+
+	wprintf(L"Stopping capture\n");
+	NmStopCapture(myCaptureEngine, adapterIndex);
+
+	NmCloseHandle(myCaptureEngine);
+	NmCloseHandle(myCapFile);
+
+	return 0;
+
+}
+
